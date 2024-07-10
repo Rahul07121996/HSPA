b5ece3f213d6167228f1647e122c803b46afc141
rahul <rahu@gmail.com>
2024-07-10 17:13:13 +0530

    Intail Commit

---
+++ Fornt/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input, OnChanges, SimpleChanges, OnInit, OnDestroy,
+  DoCheck, AfterContentInit, AfterContentChecked, 
+  AfterViewInit, AfterViewChecked } from '@angular/core';
+  
+@Component({
+  selector: 'app-root',
+  templateUrl:'./app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+
+}
