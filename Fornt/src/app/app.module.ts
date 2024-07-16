c76c20d7ae2fabf63ff0a5a0af2f543b40a2e72a
rahul <rahu@gmail.com>
2024-07-16 11:18:23 +0530

    custome directive

--- Fornt/src/app/app.module.ts
+++ Fornt/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
 import { PropertyListComponent } from './property-list/property-list.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MyDirectiveDirective } from './my-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     PropertyCardComponent,
     PropertyListComponent,
-    NavBarComponent
+    NavBarComponent,
+    MyDirectiveDirective
   ],
   imports: [
     BrowserModule,FormsModule
